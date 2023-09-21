1739a27b30a2587ad65b087565bf96aad01a2973
Yamato Matsuoka <yamaton@gmail.com>
2023-09-20 22:51:33 -0400

    Add fx

---
+++ completions/fx.fish
@@ -0,0 +1,7 @@
+# Auto-generated with h2o
+
+complete -c fx -s "h" -l "help" -d "print help"
+complete -c fx -s "v" -l "version" -d "print version"
+complete -c fx -l "themes" -d "print themes"
+complete -c fx -s "r" -l "raw" -d "treat input as a raw string"
+complete -c fx -s "s" -l "slurp" -d "read all inputs into an array"
