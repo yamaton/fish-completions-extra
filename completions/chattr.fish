a0f5579a69f25d0f05aa84b9ba1241e7861c850a
Yamato Matsuoka <yamaton@gmail.com>
2024-04-11 01:08:18 -0400

    Add chattr

---
+++ completions/chattr.fish
@@ -0,0 +1,7 @@
+# Auto-generated with h2o
+
+complete -c chattr -s "R" -d "Recursively change attributes of directories and their contents."
+complete -c chattr -s "V" -d "Be verbose with chattr's output and print the program version."
+complete -c chattr -s "f" -d "Suppress most error messages."
+complete -c chattr -s "v" -d "Set the file's version/generation number." -r
+complete -c chattr -s "p" -d "Set the file's project number." -r
